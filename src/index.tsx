a5ea90141ba880430662c8006a20fecd81cb96b8
Alen Mujanovic <alen.mujanovicc@gmail.com>
2022-08-31 23:11:13 +0200

    Implement user login,fixed user refresh token mech

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,19 @@ import "popper.js/dist/popper.js";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
 import { MainMenu, MainMenuItem } from "./components/MainMenu/MainMenu";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import ContactPage from "./components/ContactPage/ContactPage";
 import UserLoginPage from "./components/UserLoginPage/UserLoginPage";
+import CategoryPage from "./components/CategoryPage/CategoryPage";
 
 const menuItems = [
     new MainMenuItem("Home", "/"),
     new MainMenuItem("Contact", "/contact/"),
     new MainMenuItem("Log in", "/user/login/"),
+    new MainMenuItem("Cat 1", "/category/1/"),
+    new MainMenuItem("Cat 7", "/category/7/"),
+    new MainMenuItem("Cat 21", "/category/21/"),
 ];
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
@@ -24,16 +28,14 @@ root.render(
     <React.StrictMode>
         <MainMenu items={menuItems}></MainMenu>
         <HashRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/contact" element={<ContactPage />} />
-                <Route path="/user/login" element={<UserLoginPage />} />
-            </Routes>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/contact" component={ContactPage} />
+                <Route path="/user/login" component={UserLoginPage} />
+                <Route path="/category/:cId" component={CategoryPage} />
+            </Switch>
         </HashRouter>
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
